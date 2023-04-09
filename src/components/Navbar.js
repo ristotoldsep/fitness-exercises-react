d316056fdd239219097e717d386ce956b66b6a86
Risto Tõldsep <r.toldsep@gmail.com>
2023-04-09 15:11:02 +0300

    Updated final  app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,16 +8,16 @@ const Navbar = () => {
   return (
     <Stack direction="row"
         justifyContent="start"
-        sx={{ gap: { sm: '12px', xs: '40px'}, mt: { sm: '32px', xs: '20px'}, justifyContent: 'start' }} px="20px"
+        sx={{ gap: { sm: '12px', xs: '0px'}, mt: { sm: '32px', xs: '20px'}, justifyContent: 'start' }} px="0px"
     >
         <Link to="/">
             <img src={Logo} alt="logo" style={{ width: '200px', height: '120px', objectFit: 'contain', margin: '0 0px'}} />
         </Link>
         <Stack direction="row"
-            gap="40px"
-            fontSize="24px"
+            
             justifyContent="start"
             alignItems="center"
+            sx={{fontSize: {xs: '16px', sm: '24px'}, gap: {xs: '15px', sm: '40px'}}}
         >
             <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #ff2625' }}>Home</Link>
 
